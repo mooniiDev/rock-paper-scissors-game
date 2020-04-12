9bcd3c10002d6f61e23e9cde2ec38280cd8b18dd
mooniiDev <moonii@riseup.net>
2020-04-12 21:28:53 +0300

    Update functions for one round of the game

--- scripts.js
+++ scripts.js
@@ -1,39 +1,51 @@
-// Function for computer selection
-function computerPlay() {
-  const signs = ['rock', 'paper', 'scissors'];
-  return signs[Math.floor(Math.random() * signs.length)];
-}
 
-// Function for one round of the game
-function playRound(playerSign, computerSelection) {
-
-  // Validation for player selection
-  let playerSelection = playerSign.toString().toLowerCase();
-
+function playerPlay() {
+  let playerSelection = prompt('∘• rock | paper | scissors •∘').toLowerCase();
   if (playerSelection == 'rock' || playerSelection == 'paper' || playerSelection == 'scissors') {
     console.log(`Your choice is ${playerSelection}.`)
-    console.log(`Computer's choice is ${computerSelection}.`)
-  } else {
-    console.log(`You can't type ${playerSelection}.`)
-    console.log('Valid words are: rock paper scissors ლ(ಠ_ಠლ)')
+    return playerSelection;
   }
+  console.log(`YOU CAN'T TYPE ${playerSelection}.VALID WORDS ARE  ROCK | PAPER | SCISSORS`);
+  console.log('%cTHIS ROUND OF THE GAME WAS DESTROYED (╯°益°)╯彡┻━┻', 'color: #ef8513');
+}
 
-  // Selections comparison
-  switch(true) {
+function computerPlay() {
+  const signs = ['rock', 'paper', 'scissors'];
+  computerSelection = signs[Math.floor(Math.random() * signs.length)];
+  return computerSelection;
+}
+
+function oneRound(playerSelection, computerSelection) {
+  switch (true) {
     case (playerSelection == 'rock' && computerSelection == 'scissors'):
     case (playerSelection == 'paper' && computerSelection == 'rock'):
     case (playerSelection == 'scissors' && computerSelection == 'paper'):
-      console.log(`You won, because ${playerSelection} beats ${computerSelection} (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧`);
-      break;
+      playerScore++;
+      playerScore =+ playerScore;
+    console.log(`Computer's choice is ${computerSelection}.`)
+    console.log(`You won, because ${playerSelection} beats ${computerSelection} (ﾉ◕ヮ◕)ﾉ*:･ﾟ✧`);
+    console.log(`Your score is → ${playerScore} | Computer's score is → ${computerScore}`);
+    return playerScore;
     case (playerSelection == 'rock' && computerSelection == 'paper'):
     case (playerSelection == 'paper' && computerSelection == 'scissors'):
     case (playerSelection == 'scissors' && computerSelection == 'rock'):
-      console.log(`You lost, because ${computerSelection} beats ${playerSelection} ｡ﾟ･(>﹏<)･ﾟ｡`);
-      break;
+      computerScore++;
+      computerScore =+ computerScore;
+    console.log(`Computer's choice is ${computerSelection}.`)
+    console.log(`You lost, because ${computerSelection} beats ${playerSelection} ｡ﾟ･(>﹏<)･ﾟ｡`);
+    console.log(`Your score is → ${playerScore} | Computer's score is → ${computerScore}`);
+    return computerScore;
     case (playerSelection == computerSelection):
+      playerScore = playerScore;
+      console.log(`Computer's choice is ${computerSelection}.`)
       console.log(`Tie, because both chose ${playerSelection} (-_-;)・・・`);
+      console.log(`Scores remain the same: ${playerScore} | ${computerScore}`);
       break;
   }
+  return [playerScore, computerScore];
 }
 
-playRound('Rock', computerPlay());
+let playerScore = 0;
+let computerScore = 0;
+
+oneRound(playerPlay(), computerPlay());
