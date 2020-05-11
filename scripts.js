0df457e68db22d37aa4fcdbb86b0520dd56d1fd5
mooniiDev <moonii@riseup.net>
2020-05-11 22:18:57 +0300

    Add one breakpoint, update style, clean code

--- scripts.js
+++ scripts.js
@@ -1,20 +1,110 @@
 const weaponsButtons = document.querySelectorAll('.weapon-button');
-const gameOutput = document.querySelector('.game-output');
 const rounds = document.querySelector('.round');
-const computerPlayDiv = document.querySelector('.grey-border');
-const lives = document.querySelector('.lives');
 const combatText = document.querySelector('.combat-text');
 const buttonPlayAgain = document.querySelector('.play-again');
 
 let playerLives = 5;
 let computerLives = 5;
-let round = 1;
+let round = 0;
 
-playGame();
+function countRounds() {
+  round += 1;
+  rounds.innerText = `Round: ${round}`;
+  return round;
+}
+
+function computerPlay() {
+  const weapons = ['wand', 'bow', 'mace'];
+  const computerSelection = weapons[Math.floor(Math.random() * weapons.length)];
+  const computerIcon = document.querySelector('.computer-icon');
+
+  computerIcon.classList.remove('fa-skull');
+  if (computerSelection === 'wand') {
+    computerIcon.classList.add('fa-wand-magic');
+    computerIcon.style.color = '#8070ac';
+  } else if (computerSelection === 'bow') {
+    computerIcon.classList.add('fa-bow-arrow');
+    computerIcon.style.color = '#62b49c';
+  } else if (computerSelection === 'mace') {
+    computerIcon.classList.add('fa-mace');
+    computerIcon.style.color = '#b96b78';
+  }
+  return computerSelection;
+}
+
+function countLives(playerSelection, computerSelection) {
+  const gameOutput = document.querySelector('.game-output');
+  const computerPlayDiv = document.querySelector('.computer-play-div');
+
+  switch (true) {
+    case (playerSelection === 'wand' && computerSelection === 'mace'):
+    case (playerSelection === 'bow' && computerSelection === 'wand'):
+    case (playerSelection === 'mace' && computerSelection === 'bow'):
+      combatText.textContent = `Impressive attack! The enemy lost one life, because the great power of your ${playerSelection} crushed his ${computerSelection}!`;
+      gameOutput.style.border = '4px solid #62b49c';
+      computerPlayDiv.classList.remove('grey-border', 'red-border', 'purple-border');
+      computerPlayDiv.classList.add('green-border');
+      computerLives -= 1;
+      break;
+    case (playerSelection === 'wand' && computerSelection === 'bow'):
+    case (playerSelection === 'bow' && computerSelection === 'mace'):
+    case (playerSelection === 'mace' && computerSelection === 'wand'):
+      combatText.innerText = `Unfortunate defeat.. You lost one life, because your ${playerSelection} lacks of power against enemy's ${computerSelection}!`;
+      gameOutput.style.border = '4px solid #b96b78';
+      computerPlayDiv.classList.remove('grey-border', 'green-border', 'purple-border');
+      computerPlayDiv.classList.add('red-border');
+      playerLives -= 1;
+      break;
+    case (playerSelection === computerSelection):
+      combatText.innerText = `Hmm.. Two ${playerSelection}s means a draw, so no lives were lost. Let's try again.`;
+      gameOutput.style.border = '4px solid #8070ac';
+      computerPlayDiv.classList.remove('grey-border', 'green-border', 'red-border');
+      computerPlayDiv.classList.add('purple-border');
+      break;
+      default:
+        return;
+  }
+
+  const lives = document.querySelector('.lives');
+  lives.innerText = `Your Lives: ${playerLives} ︱ Enemy's Lives: ${computerLives}`;
+  return [playerLives, computerLives];
+}
+
+function endGame(playerHealth, computerHealth) {
+  if (playerHealth === 0 || computerHealth === 0) {
+    for (let i = 0; i < weaponsButtons.length; i++) {
+      weaponsButtons[i].disabled = true;
+      weaponsButtons[i].classList.add('disabled-button');
+      weaponsButtons[i].style.opacity = '0.5';
+    }
+
+    const computerIcon = document.querySelector('.computer-icon');
+    computerIcon.style.opacity = '0.5';
+
+    const gameEndText = document.querySelector('.game-end-text');
+    if (playerLives > computerLives) {
+      combatText.innerText = 'Hehe, poor enemy has no life left.. He barely holds himself in one piece.';
+      gameEndText.textContent = 'You Won This Battle!';
+      gameEndText.style.color = '#62b49c';
+    } else {
+      combatText.innerText = 'Ouch.. No life left for you. Enjoy the mocking laughter of the enemy.';
+      gameEndText.textContent = 'You Lost This Battle!';
+      gameEndText.style.color = '#b96b78';
+    }
+    buttonPlayAgain.style.visibility = 'visible';
+  }
+}
+
+function resetGame() {
+  buttonPlayAgain.addEventListener('click', () => {
+    window.location.reload();
+  });
+}
 
 function playGame() {
+  let playerSelection;
   weaponsButtons.forEach((weapon) => {
-    weapon.addEventListener('click', function() {
+    weapon.addEventListener('click', () => {
       const weaponIcons = document.querySelectorAll('.weapon-icon');
       if (weapon.classList.contains('wand-button')) {
         weaponIcons[0].style.color = '#8070ac';
@@ -38,90 +128,6 @@ function playGame() {
       resetGame();
     });
   });
-  return;
-};
+}
 
-function countRounds() {
-  rounds.innerText = `Round: ${round}`;
-  round =+ round;
-  round++;
-  return round;
-};
-
-function computerPlay() {
-  const weapons = ['wand', 'bow', 'mace'];
-  const computerSelection = weapons[Math.floor(Math.random() * weapons.length)];
-  const computerIcon = document.querySelector('.computer-icon');
-  computerIcon.classList.remove('fa-skull');
-  if (computerSelection === 'wand') {
-    computerIcon.classList.add('fa-wand-magic');
-    computerIcon.style.color = '#8070ac';
-  } else if (computerSelection === 'bow') {
-    computerIcon.classList.add('fa-bow-arrow');
-    computerIcon.style.color = '#62b49c';
-  } else if (computerSelection === 'mace'){
-    computerIcon.classList.add('fa-mace');
-    computerIcon.style.color = '#b96b78';
-  }
-  return computerSelection;
-};
-
-function countLives(playerSelection, computerSelection) {
-  switch (true) {
-    case (playerSelection == 'wand' && computerSelection == 'mace'):
-    case (playerSelection == 'bow' && computerSelection == 'wand'):
-    case (playerSelection == 'mace' && computerSelection == 'bow'):
-      combatText.textContent = `Impressive Attack! The enemy lost one life, because the great power of your ${playerSelection} crushed his ${computerSelection}!`;
-      gameOutput.style.border = '4px solid #62b49c';
-      computerPlayDiv.classList.remove('grey-border', 'red-border', 'purple-border');
-      computerPlayDiv.classList.add('green-border');
-      computerLives--;
-      break;
-    case (playerSelection == 'wand' && computerSelection == 'bow'):
-    case (playerSelection == 'bow' && computerSelection == 'mace'):
-    case (playerSelection == 'mace' && computerSelection == 'wand'):
-      combatText.innerText = `Unfortunate Defeat.. You lost one life, because your ${playerSelection} lacks of power against enemy's ${computerSelection}!`;
-      gameOutput.style.border = '4px solid #b96b78';
-      computerPlayDiv.classList.remove('grey-border', 'green-border', 'purple-border');
-      computerPlayDiv.classList.add('red-border');
-      playerLives--;
-      break;
-    case (playerSelection == computerSelection):
-      combatText.innerText = `Hmm.. Two ${playerSelection}s means a draw, so no lives were lost. Let's try again.`;
-      gameOutput.style.border = '4px solid #8070ac';
-      computerPlayDiv.classList.remove('grey-border', 'green-border', 'red-border');
-      computerPlayDiv.classList.add('purple-border');
-      break;
-  }
-  lives.innerText = `Your Lives: ${playerLives} ︱ Enemy Lives: ${computerLives}`;
-  return [playerLives, computerLives];
-};
-
-function endGame(playerLives, computerLives) {
-  const gameEndText = document.querySelector('.game-end-text');
-  if (playerLives === 0 || computerLives === 0) {
-    for(let i = 0; i < weaponsButtons.length; i++) {
-      weaponsButtons[i].disabled = true;
-      weaponsButtons[i].classList.add('disabled-button');
-      weaponsButtons[i].style.opacity = '0.5';
-    };
-    if (playerLives > computerLives) {
-      combatText.innerText = 'Hehe, poor enemy has no life left.. He barely holds himself in one piece.';
-      gameEndText.textContent = 'You Won This Battle!';
-      gameEndText.style.color = '#62b49c';
-    } else {
-      combatText.innerText = 'Ouch.. No life left for you. Enjoy the mocking enemy laughter.';
-      gameEndText.textContent = 'You Lost This Battle!';
-      gameEndText.style.color = '#b96b78';
-    };
-    buttonPlayAgain.style.visibility = 'visible';
-  };
-  return;
-};
-
-function resetGame() {
-  buttonPlayAgain.addEventListener('click', function() {
-    window.location.reload();
-  });
-  return;
-};
+playGame();
